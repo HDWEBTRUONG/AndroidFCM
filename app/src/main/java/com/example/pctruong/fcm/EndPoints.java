2fcf94f06ac77b402dfe8b12466e36e2885e0772
PhamTruong <truong.pham@hdwebsoft.co>
2018-06-05 09:38:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pctruong/fcm/EndPoints.java
@@ -0,0 +1,13 @@
+package com.example.pctruong.fcm;
+
+/**
+ * Created by PCTruong on 29/05/2018.
+ */
+
+public class EndPoints {
+    public static final String URL_REGISTER_DEVICE = "http://192.168.1.90/fcm/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "http://192.168.1.90/fcm/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://192.168.1.90/fcm/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "http://192.168.1.90/fcm/GetRegisteredDevices.php";
+}
+
---
+++ fcm/Config.php
@@ -0,0 +1,7 @@
+<?php 
+ define('DB_USERNAME','root');
+ define('DB_PASSWORD','');
+ define('DB_NAME','fcm');
+ define('DB_HOST','localhost');
+
+ define('FIREBASE_API_KEY', 'AIzaSyBtbklp6voqoK5_dnEo1u1wXza9GUr9AIE');
